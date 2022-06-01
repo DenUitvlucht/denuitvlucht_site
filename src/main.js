673373a0e76166e4f1ccee658d264c429d79780f
DaanVervacke <daan.vervacke@sgsintpaulus.eu>
2022-06-01 19:49:29 +0200

    Firestore basics

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
-import VueFriendlyIframe from 'vue-friendly-iframe';
+
 
 loadFonts()
 
 createApp(App).use(router)
   .use(vuetify)
-  .use(VueFriendlyIframe)
   .mount('#app')
