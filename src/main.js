4864aabc49d2cae9946ae9e98daa4b6fc9c54053
DaanVervacke <daan.vervacke@sgsintpaulus.eu>
2022-06-02 23:49:46 +0200

    test

--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 
-const VITE_FB_API_KEY = import.meta.env.VITE_FB_API_KEY
-const VITE_FB_AUTH_DOMAIN = import.meta.env.VITE_FB_AUTH_DOMAIN
-const VITE_FB_DB_URL = import.meta.env.VITE_FB_DB_URL
-const VITE_FB_PROJECT_ID = import.meta.env.VITE_FB_PROJECT_ID
-const VITE_FB_STORAGE_BUCKET = import.meta.env.VITE_FB_STORAGE_BUCKET
-const VITE_FB_MESSAGING_SENDER_ID = import.meta.env.VITE_FB_MESSAGING_SENDER_ID
-const VITE_FB_APP_ID = import.meta.env.VITE_FB_APP_ID
-const VITE_MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID
+
+const VITE_FB_API_KEY = Buffer.from(import.meta.env.VITE_FB_API_KEY, 'base64').toString('utf-8')
+const VITE_FB_AUTH_DOMAIN = Buffer.from(import.meta.env.VITE_FB_AUTH_DOMAIN, 'base64').toString('utf-8')
+const VITE_FB_DB_URL = Buffer.from(import.meta.env.VITE_FB_DB_URL, 'base64').toString('utf-8')
+const VITE_FB_PROJECT_ID = Buffer.from(import.meta.env.VITE_FB_PROJECT_ID, 'base64').toString('utf-8')
+const VITE_FB_STORAGE_BUCKET = Buffer.from(import.meta.env.VITE_FB_STORAGE_BUCKET, 'base64').toString('utf-8')
+const VITE_FB_MESSAGING_SENDER_ID = Buffer.from(import.meta.env.VITE_FB_MESSAGING_SENDER_ID, 'base64').toString('utf-8')
+const VITE_FB_APP_ID = Buffer.from(import.meta.env.VITE_FB_APP_ID, 'base64').toString('utf-8')
+const VITE_MEASUREMENT_ID = Buffer.from(import.meta.env.VITE_MEASUREMENT_ID, 'base64').toString('utf-8')
 
 console.log(VITE_FB_PROJECT_ID)
 
