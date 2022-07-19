6b141e642fa4c78352926daa8f9a3b27ce76ae78
DaanVervacke <daan.vervacke@sgsintpaulus.eu>
2022-07-19 21:23:15 +0200

    test

--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 import { encode, decode } from 'js-base64';
 
-const VITE_FB_API_KEY = decode(import.meta.env.VITE_FB_API_KEY)
-const VITE_FB_AUTH_DOMAIN = decode(import.meta.env.VITE_FB_AUTH_DOMAIN)
-const VITE_FB_DB_URL = decode(import.meta.env.VITE_FB_DB_URL)
-const VITE_FB_PROJECT_ID = decode(import.meta.env.VITE_FB_PROJECT_ID)
-const VITE_FB_STORAGE_BUCKET = decode(import.meta.env.VITE_FB_STORAGE_BUCKET)
-const VITE_FB_MESSAGING_SENDER_ID = decode(import.meta.env.VITE_FB_MESSAGING_SENDER_ID)
-const VITE_FB_APP_ID = decode(import.meta.env.VITE_FB_APP_ID)
-const VITE_MEASUREMENT_ID = decode(import.meta.env.VITE_MEASUREMENT_ID)
+const VITE_FB_API_KEY = import.meta.env.VITE_FB_API_KEY
+const VITE_FB_AUTH_DOMAIN = import.meta.env.VITE_FB_AUTH_DOMAIN
+const VITE_FB_DB_URL = import.meta.env.VITE_FB_DB_URL
+const VITE_FB_PROJECT_ID = import.meta.env.VITE_FB_PROJECT_ID
+const VITE_FB_STORAGE_BUCKET = import.meta.env.VITE_FB_STORAGE_BUCKET
+const VITE_FB_MESSAGING_SENDER_ID = import.meta.env.VITE_FB_MESSAGING_SENDER_ID
+const VITE_FB_APP_ID = import.meta.env.VITE_FB_APP_ID
+const VITE_MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID
 
 const firebaseConfig = {
   apiKey: VITE_FB_API_KEY,
